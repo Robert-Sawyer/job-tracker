08adf5415947912d318bb8d5e873b2a916be4c88
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2026-07-26 06:38:19 +0200

    Add Eslint and prettier config

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import tsEslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tsEslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.next/**",
+      "**/build/**"
+    ],
+  },
+
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    extends: [js.configs.recommended],
+  },
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [
+      js.configs.recommended,
+      ...tsEslint.configs.recommended,
+    ],
+  },
+
+  eslintConfigPrettier
+);
